6cb950907361e10765d83960589de00d529e187b
SaaifKhan <saif_khan09@hotmail.com>
2021-10-25 11:33:52 +0500

    User Current Location with Marker set and Zoom using fusedLocation

--- app/src/main/java/com/dotinfiny/mapsapp/MainActivity.kt
+++ app/src/main/java/com/dotinfiny/mapsapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.dotinfiny.mapsapp
 
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -9,13 +14,22 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
     lateinit var myMap:GoogleMap
+
+    lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //permission will be granted
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION),100)
 
 
         val mapFragment = supportFragmentManager
@@ -24,13 +38,28 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
 
         button.setOnClickListener {
-            val latLng = LatLng(24.944796314820035, 67.04499089537165)
+            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-            val markerOption:MarkerOptions =  MarkerOptions().position(latLng).title("ABC")
+            mFusedLocationProviderClient.lastLocation.addOnSuccessListener {loc:Location? ->
 
-            myMap.addMarker(markerOption)
-            myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,12.0f))
+                val latLng = LatLng(loc!!.latitude,loc.longitude)
+
+                myMap.addMarker(MarkerOptions().position(latLng).title("Found Location"))
+
+                myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,100f))
+
+            }
         }
+
+
+//        button.setOnClickListener {
+//            val latLng = LatLng(24.944796314820035, 67.04499089537165)
+//
+//            val markerOption:MarkerOptions =  MarkerOptions().position(latLng).title("ABC")
+//
+//            myMap.addMarker(markerOption)
+//            myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,12.0f))
+//        }
     }
 
     override fun onMapReady(gMap: GoogleMap) {
